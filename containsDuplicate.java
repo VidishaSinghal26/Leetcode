5647401f16b050531d0840efc62bd4617a6b1430
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2023-03-12 20:36:41 +0530

    leetcode-217

---
+++ containsDuplicate.java
@@ -0,0 +1,28 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+Solution:
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            if(map.containsKey(i)){
+                return true;
+            }
+            map.put(i,1);
+        }
+        return false;
+    }
+}
