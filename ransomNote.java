4336de86f291468a3b62e847e01c7e1959884ec3
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2023-03-14 19:19:55 +0530

    leetcode-383

---
+++ ransomNote.java
@@ -0,0 +1,45 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+Solution:
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) 
+           return false;
+        int c =0;
+        char[] arr = ransomNote.toCharArray();
+        for(int i=0 ; i<ransomNote.length() ; i++){
+            if(magazine.contains(""+ransonNote.indesOf(i))){
+               magazine =  magazine.replaceFirst(""+arr[i],"");
+                c++;
+            }
+        }
+        if(c==ransomNote.length())
+          return true;
+        return false;
+
+// Optimized Appoarch
+
+		if (ransomNote.length() > magazine.length()) 
+           return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
