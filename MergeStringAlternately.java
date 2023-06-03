0e1a58ebe479ea971f33f9ec0371aec0aeefe011
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2023-06-03 19:36:26 +0530

    leetcode-1768

--- MergeStringAlternately.java
+++ MergeStringAlternately.java
@@ -27,7 +27,19 @@
 
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
+        
+//Method -1 (By StringBuilder) 
+        
+        StringBuilder sb = new StringBuilder();
+        int min = Integer.min(word1.length(), word2.length());
+        
+        for(int i = 0; i < min; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));            
+        }
+        return sb.append(word1.substring(min)).append(word2.substring(min)).toString();
+        
+// Method -2
         
         if(word1.length() == 0){
             return word2;
