846538450d295c8e16f43a20a948d7289efdfd04
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2023-09-08 18:29:27 +0530

    leetcode-203

---
+++ LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,48 @@
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+ 
+// Example 1:
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+
+// Example 2:
+// Input: head = [], val = 1
+// Output: []
+
+// Example 3:
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return head;
+        } 
+
+        ListNode curr = head;
+        while(curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }
+            else{
+                curr = curr.next;
+            }
+        }
+
+        if(head.val == val){
+            return head.next;
+        }
+
+        return head;
+    }
+}
