00143c7346af358d1cb03514c6d9b4b6b2663cc7
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2023-09-10 17:28:19 +0530

    leetcode-338

---
+++ BitManipulation/CountingBits.java
@@ -0,0 +1,60 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+// Example 1:
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+  
+// Example 2:
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+class Solution {
+    public int[] countBits(int n) {
+
+        int dp[] = new int[n+1];
+        for(int i = 1 ; i<=n ;i++){
+            if( (i&1) == 0){
+                dp[i] = dp[i>>1];
+            }
+            else{
+                dp[i] = dp[i>>1] + i%2;
+            }
+        }
+        return dp;
+    }
+}
+
+//OR
+
+class Solution {
+    public int[] countBits(int n) {
+        int dp[] = new int[n+1];
+        for(int i = 1 ; i<=n ;i++){
+             dp[i] = dp[i>>1] + i%2;
+        }
+        return dp;
+    }
+}
+
+//OR
+
+class Solution {
+    public int[] countBits(int n) {
+        int dp[] = new int[n+1];
+        for(int i = 1 ; i<=n ;i++){
+            dp[i] = Integer.bitCount(i);
+        }
+        return dp;
+    }
+}
