fb0a27159216443738d00ea7ef7f40a15e0e632a
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2023-09-16 13:18:12 +0530

    leetcode-82

---
+++ LinkedList/RemoveDuplicatesfromSortedListlI.java
@@ -0,0 +1,45 @@
+// Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+// Example 1:
+// Input: head = [1,2,3,3,4,4,5]
+// Output: [1,2,5]
+
+// Example 2:
+// Input: head = [1,1,1,2,3]
+// Output: [2,3]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode prev= dummy;
+        ListNode curr = head;
+        while(curr != null){
+            if(curr.next != null && curr.val == curr.next.val){
+                while(curr.next != null && curr.val == curr.next.val){
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
